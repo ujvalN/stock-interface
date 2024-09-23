d0acf163dcab970528ad7ed57c1e1aec26d8fa00
ujvalN <ujnallur@gmail.com>
2024-09-22 19:48:39 -0500

    added basic functionality

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import API
-
+import yfinance as yf
 
 def main():
+    print("Welcome to the Stock Info Interface, you will get the stock price history of whichever stock you enter!")
+    stock: str = input("Stock: ")
+    history: str = input("History (only '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', or 'max'): ")
+    stock_price = getData(stock, history) 
 
+    print(f"Price of {stock} {history} ago: ${stock_price.iloc[0]:.2f}")
 
-
+def getData(stock, history):
+    stock = yf.Ticker(stock)
+    data = stock.history(period=(f"{history}"))
+    
+    return data['Close']
+    
 if __name__ == "__main__":
     main()
